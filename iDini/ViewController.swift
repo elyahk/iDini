c3e3661ab03241017b56b9a8553613d3a7e94b5d
Eldorbek Nusratov <eldorbek.nusratov.98@gmail.com>
2022-10-27 22:13:12 +0200

    Initial Commit

---
+++ iDini/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iDini
+//
+//  Created by eldorbek nusratov on 10/27/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
